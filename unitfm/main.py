9678bc7740d0b7c2307bfb9de1278c23fd128bfa
Karsten Jeschkies <jeskar@web.de>
2017-12-17 20:59:46 +0100

    Post status for commit to Github.

--- unitfm/main.py
+++ unitfm/main.py
@@ -1,6 +1,10 @@
+import aiohttp
+import os
 from aiohttp import web
 import xml.etree.ElementTree as ET
 
+gh_user = os.environ.get('GITHUB_USER', None)
+gh_token = os.environ.get('GITHUB_TOKEN', None)
 
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
@@ -8,15 +12,30 @@ async def handle(request):
     return web.Response(text=text)
 
 
-async def view_junit(request):
+async def update_commit_status(owner, repo, sha):
+    auth = aiohttp.BasicAuth(gh_user, gh_token)
+    async with aiohttp.ClientSession(auth=auth) as session:
+        url = 'https://api.github.com/repos/{}/{}/statuses/{}'.format(owner, repo, sha)
+        data = {'state': 'pending', 'target_url': 'http://localhost:8000/', 'context': 'test/unit'}
+        async with session.post(url, json=data) as response:
+            response.raise_for_status()
+
+async def post_junit(request):
     junit = ET.parse('pytest.xml').getroot()
     page = 'Errors: {}'.format(junit.get('errors'))
-    return web.Response(text=page)
+
+    owner = request.match_info.get('owner', None)
+    repo = request.match_info.get('repo', None)
+    commit_sha = request.match_info.get('sha', None)
+    await update_commit_status(owner, repo, commit_sha)
+
+    return web.Response(status=201)
+
 
 def app():
     app_ = web.Application()
     app_.router.add_get('/', handle)
-    app_.router.add_get('/view', view_junit)
+    app_.router.add_post('/{owner}/{repo}/commit/{sha}', post_junit)
     return app_
 
 
