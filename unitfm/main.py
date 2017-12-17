aa77823dbbdf6b85aba008ec0aec1056be290dc8
Karsten Jeschkies <jeskar@web.de>
2017-12-17 19:45:56 +0100

    Use aiohttp-devtools to serve errors from junit file.

--- unitfm/main.py
+++ unitfm/main.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+import xml.etree.ElementTree as ET
 
 
 async def handle(request):
@@ -7,10 +8,15 @@ async def handle(request):
     return web.Response(text=text)
 
 
+async def view_junit(request):
+    junit = ET.parse('pytest.xml').getroot()
+    page = 'Errors: {}'.format(junit.get('errors'))
+    return web.Response(text=page)
+
 def app():
     app_ = web.Application()
     app_.router.add_get('/', handle)
-    app_.router.add_get('/{name}', handle)
+    app_.router.add_get('/view', view_junit)
     return app_
 
 
