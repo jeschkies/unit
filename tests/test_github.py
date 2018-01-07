afba6aff788bb7917dee8df6fe2829855c1f83c5
Karsten Jeschkies <jeskar@web.de>
2018-01-07 20:20:08 +0100

    Test headers of different Github sessions.

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,5 +1,5 @@
 """Test Github API client."""
-from unitfm.github import SessionManager
+from unitfm.github import (AccessTokenSession, BasicAuthenticatedSession, SessionManager)
 import pytest
 
 
@@ -21,3 +21,17 @@ def test_jwt_generate(key, jwt):
     """Verify that JWT is correct."""
     auth = SessionManager(key, 1234)
     assert auth._generate_jwt(1515102490, 300) == jwt
+
+
+async def test_basic_authentication():
+    """Verify basic authentication header."""
+    session = BasicAuthenticatedSession('jeschkies', 'super secret')
+    header = await session.auth_header_value()
+    assert header == 'Basic amVzY2hraWVzOnN1cGVyIHNlY3JldA=='
+
+
+async def test_access_token_session():
+    """Verify access token header."""
+    session = AccessTokenSession('super token')
+    header = await session.auth_header_value()
+    assert header == 'token super token'
