1e88ad673afb5add6933df6998369034ec7421bb
Karsten Jeschkies <k@jeschkies.xyz>
2018-09-30 20:33:21 +0200

    Migrate to Gradle. (#15)

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(name='unitfm',
-      version='0.0.1',
-      packages=find_packages(),
-      package_data={
-          '': ['templates/*.html', 'static/*.*']
-      },
-      include_package_data=True)
--- unitfm/store/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Different storage backends for unit files."""
-from .b2 import B2Store  # NOQA F401
-from .file import FileStore  # NOQA F401
