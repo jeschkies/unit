a08592bf36f725ab35bd60d6df5c6f715fe93494
Karsten Jeschkies <k@jeschkies.xyz>
2018-10-03 18:53:23 +0200

    Accept simple JSON object.

--- src/test/kotlin/UnitApp.kt
+++ src/test/kotlin/UnitApp.kt
@@ -4,20 +4,34 @@ import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
+import io.ktor.jackson.JacksonConverter
+import io.ktor.request.receive
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+
+data class Test(val result: String)
 
 fun Application.module() {
     install(DefaultHeaders)
     install(CallLogging)
+    install(ContentNegotiation) {
+        register(ContentType.Application.Json, JacksonConverter())
+    }
     install(Routing) {
         get("/") {
             call.respondText("My Example Blog", ContentType.Text.Html)
+        }
+        post("/") {
+            val test = call.receive<Test>()
+            val testResult = test.result
+            call.respondText(testResult, ContentType.Text.Html)
         }
     }
 }
